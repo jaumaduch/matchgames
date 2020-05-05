39a60319adccd30beac7d754b659af50bd7b0258
jaumaduch <40838921+jaumaduch@users.noreply.github.com>
2020-05-04 22:18:43 -0300

    Criando o maper do Entity

---
+++ XGame_Infra/Persistence/Map/MapJogador.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using XGame_Domain.Entities;
+
+namespace XGame_Infra.Persistence.Map
+{
+    public class MapJogador : EntityTypeConfiguration<Jogador>
+    {
+        public MapJogador()
+        {
+            ToTable("TB_JOGADOR");
+
+            Property(p => p.Email.EnderecoEmail).HasMaxLength(200).IsRequired().HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("UK_JOGADOR_EMAIL") { IsUnique = true})).HasColumnName("EMAIL");
+            Property(p => p.Nome.PrimeiroNome).HasMaxLength(200).IsRequired().HasColumnName("PRIMEIRO_NOME");
+            Property(p => p.Nome.UltimoNome).HasMaxLength(200).IsRequired().HasColumnName("SEGUNDO_NOME");
+            Property(p => p.Senha).IsRequired();
+            Property(p => p.Status).IsRequired();
+
+        }
+    }
+}
---
+++ XGame_Infra/Persistence/XGameContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using XGame_Domain.Entities;
+
+namespace XGame_Infra.Persistence
+{
+    public class XGameContext : DbContext
+    {
+        public XGameContext() : base("XGameConnection")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public IDbSet<Jogador> Jogadores { get; set; }
+
+        public IDbSet<Plataformas> Plataformas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength (100));
+
+            modelBuilder.Configurations.AddFromAssembly(typeof(XGameContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
